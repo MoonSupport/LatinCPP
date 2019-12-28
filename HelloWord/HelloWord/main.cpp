bc31f1d761d62ca713df5fe7ef6dbe07f000ebf4
ClockMoon <jiwon3346@naver.com>
2019-12-28 22:42:38 +0900

    hello world

---
+++ HelloWord/HelloWord/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWord
+//
+//  Created by 문지원 on 28/12/2019.
+//  Copyright © 2019 문지원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
