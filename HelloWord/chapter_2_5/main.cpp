3487484bf2cdbc66672ad376845e574b49dc1fb3
ClockMoon <jiwon3346@naver.com>
2019-12-29 21:47:40 +0900

    chapter2 constants

---
+++ HelloWord/chapter_2_5/main.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  chapter_2_5
+//
+//  Created by 문지원 on 29/12/2019.
+//  Copyright © 2019 문지원. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+int main(int argc, const char * argv[]) {
+    using namespace std;
+    
+    float f(3.141592f);
+    double d(3.141592);
+    long double ld(3.141592);
+    
+    cout << 3.14 << endl;
+    cout << 31.4e-1 << endl;
+    cout << 31.4e-2 << endl;
+    cout << 31.4e1 << endl;
+    cout << 31.4e2 << endl<<endl;
+    
+    cout << std::setprecision(2);
+    cout << 1.0 / 3.0 << endl;
+    cout << 1.0 / 3.0 << endl;
+
+
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(long double) << endl<<endl;
+    
+    cout << numeric_limits<float>::max() << endl;
+    cout << numeric_limits<double>::max() << endl;
+    cout << numeric_limits<long double>::max() << endl<<endl;
+
+    cout << numeric_limits<float>::min() << endl;
+    cout << numeric_limits<double>::min() << endl;
+    cout << numeric_limits<long double>::min() << endl<<endl;
+    
+    cout << numeric_limits<float>::lowest() << endl;
+    cout << numeric_limits<double>::lowest() << endl;
+    cout << numeric_limits<long double>::lowest() << endl<<endl;
+        
+    return 0;
+}
