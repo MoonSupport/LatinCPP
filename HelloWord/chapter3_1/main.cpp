457c9a50bee886446f3862150278ff74ae325a7b
ClockMoon <jiwon3346@naver.com>
2019-12-30 23:46:32 +0900

    operator

---
+++ HelloWord/chapter3_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chapter3_1
+//
+//  Created by 문지원 on 30/12/2019.
+//  Copyright © 2019 문지원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int operatorOrder = 4 + 2 * 3;
+    std::cout << "what us the first" << operatorOrder << std::endl;
+    return 0;
+}
