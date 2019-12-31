803fc75d175b351c0c3e1b3f456009b289aabab9
ClockMoon <jiwon3346@naver.com>
2019-12-31 19:38:27 +0900

    bit

---
+++ HelloWord/chapter_binary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chapter_binary
+//
+//  Created by 문지원 on 31/12/2019.
+//  Copyright © 2019 문지원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
