109724aa0b7264c97b5997e538d2947c3b8becf1
ClockMoon <jiwon3346@naver.com>
2019-12-31 18:30:59 +0900

    operater

---
+++ HelloWord/chapter3_operator/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  chapter3_operator
+//
+//  Created by 문지원 on 31/12/2019.
+//  Copyright © 2019 문지원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int a =  true;
+    int b = false;
+    std::cout << (a && b) << std::endl;
+    return 0;
+}
