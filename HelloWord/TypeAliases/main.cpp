8ea35329f23d251554c47bc1ac12d8a86d718362
ClockMoon <jiwon3346@naver.com>
2020-01-04 17:21:37 +0900

    type alias

--- HelloWord/TypeAliases/main.cpp
+++ HelloWord/TypeAliases/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cstdint>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    using namespace std;
+    
+    typedef double distance_t;
+    
+    double my_distance;
+    distance_t home2work; // 위나 아래나 같다.
+    distance_t home2school;
+    
+    
+    int8_t i(97); // 와 같은 역할을 alias가 한다.
+    
+    vector<pair<string, int>> pairlist1; // bracet를 덕지덕지 써도 결국 자료형인거고, 자료형 안에 자료형을 잡아넣는 것일 뿐이다.
+    vector<pair<string, int>> pairlist2;
+    
+    typedef vector<pair<string, int>> pairlist;
+    
+    pairlist pairlist3;
+    pairlist pairlist4;
+
+    using pairlist_2 = vector<pair<string, int>>;
+    
+    pairlist_2 pairlist5;
+    pairlist_2 pairlist6;
+
+    
+    
+    
     return 0;
 }
