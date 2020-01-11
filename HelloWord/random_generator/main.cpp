aeca160796a29158f89612135a999617102dfb42
ClockMoon <jiwon3346@naver.com>
2020-01-11 15:31:09 +0900

    random gene

--- HelloWord/random_generator/main.cpp
+++ HelloWord/random_generator/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include <iostream>
+#include <ctime>
+
+unsigned int PRNG(){ // 컴터는 난수를 생성할 수 없다. 그래서 대신 예상치 못한 숫자를 만들어 내서 낸다.
+                     // 대신 시드 넘버를 바꿔주며 그 수를 변경해준다. 그리고 그 숫자는 time(시간)에 따라 변동시킨다.
+    static unsigned int seed = static_cast<unsigned int>(std::time(0));
+    
+    seed = 8253729 * seed + 2396403;
+    
+    return seed % 32768;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    using namespace std;
+    
+    for(int count = 1; count <= 100; ++count) {
+        
+        cout << PRNG() << "\t";
+        
+        if(count % 5 == 0) cout << endl;
+        
+    }
+    
     return 0;
 }
