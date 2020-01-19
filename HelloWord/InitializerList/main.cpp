a291019e941abc41592714d3b40dd0b7bc4374cd
ClockMoon <jiwon3346@naver.com>
2020-01-19 17:35:47 +0900

    initializerList

---
+++ HelloWord/InitializerList/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  InitializerList
+//
+//  Created by 문지원 on 19/01/2020.
+//  Copyright © 2020 문지원. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class Something{
+    private :
+    int _i;
+    double _d;
+    char _c;
+    
+    public :
+    Something()
+        :_i(1), _d(3.14), _c('a')
+    {
+        _i *=3;
+        _d *=3;
+        _c +=3;
+    }
+    
+    void print(){
+        cout << _i << " " << _d << " " << _c << endl;
+    };
+};
+
+int main(int argc, const char * argv[]) {
+    Something some;
+    some.print();
+    return 0;
+}
