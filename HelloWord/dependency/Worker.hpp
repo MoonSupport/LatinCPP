aacdaf43b3bf2aea6d322d01f0e9b8c5d880cf87
ClockMoon <jiwon3346@naver.com>
2020-02-01 17:41:06 +0900

    dependency

---
+++ HelloWord/dependency/Worker.hpp
@@ -0,0 +1,14 @@
+//
+//  Worker.hpp
+//  dependency
+//
+//  Created by 문지원 on 01/02/2020.
+//  Copyright © 2020 문지원. All rights reserved.
+//
+
+#ifndef Worker_hpp
+#define Worker_hpp
+
+#include <stdio.h>
+
+#endif /* Worker_hpp */
