a8357b3a53b672f5daf3a7451cb79620abd89c76
ClockMoon <jiwon3346@naver.com>
2020-02-16 18:28:22 +0900

    dangerous Exception

--- HelloWord/dangerousException/main.cpp
+++ HelloWord/dangerousException/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, const char * argv[]) {
         int* i = new int[10000000];
         unique_ptr<int> up_i(i);
         
-        throw 'error';
+        throw 'error'; // 에러를 타서 delete 못해주는 경우를 스마트 포인터로 방지
     }catch(char& e) {
         cout << e << endl;
     }
